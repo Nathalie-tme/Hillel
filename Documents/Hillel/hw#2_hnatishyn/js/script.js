261127745d867765340a64f9c0314f2381595665
Nataliia <nataliia.hnatishyn@gmail.com>
2021-06-09 10:47:02 +0300

    4th commit

--- Documents/Hillel/hw#2_hnatishyn/js/script.js
+++ Documents/Hillel/hw#2_hnatishyn/js/script.js
@@ -1,31 +1,15 @@
-/*const a = prompt("Введите первое число " + " ");
-const b = prompt("Введите второе число " + " ");
-
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-*/
-
-
-const a = prompt("Введите первое число" + " ");
-const b = prompt("Введите второе число" + " ");
+const a = +prompt("Введите первое число" + " ");
+const b = Number(prompt("Введите второе число" + " "));
 let action = prompt("Какое математическое действие будем выполнять?" + " ");
 
-if (action == "+"){
+if (action === "+") {
     console.log(a + b);
-} else if(action == "-"){
+} else if(action === "-") {
     console.log(a - b);
-} else if(action == "*"){
+} else if(action === "*") {
     console.log(a * b);
-} else if(action == "/"){
+} else if(action === "/") {
     console.log(a / b);
-} else if(action !=="*"){
-    alert("Мы не можем выполнить даную команду " + action );
-} else if(action !=="+"){
-    alert("Мы не можем выполнить даную команду " + action );
-}else if(action !=="-"){
-    alert("Мы не можем выполнить даную команду " + action );
-} else if(action !=="/"){
-    alert("Мы не можем выполнить даную команду " + action );
+} else {
+    alert(`Мы не можем выполнить даную команду ${action}`);
 }
